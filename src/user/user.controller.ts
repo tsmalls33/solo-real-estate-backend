ca8ed45277acbc87f549051fe4ea8dc6f8563151
Thomas Smallwood <tbm.smallwood@gmail.com>
2025-08-09 11:47:09 +0200

    All CRUD user routes. wip: error handling

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -4,12 +4,12 @@ import {
   Body,
   Get,
   Param,
-  // Patch,
-  // Delete
+  Put,
+  Delete
 } from '@nestjs/common';
 import { UserService } from './user.service';
 import { CreateUserDto } from './dto/create-user.dto';
-// import { UpdateUserDto } from './dto/update-user.dto';
+import { UpdateUserDto } from './dto/update-user.dto';
 
 @Controller('user')
 export class UserController {
@@ -30,13 +30,13 @@ export class UserController {
     return this.userService.findOne(id);
   }
 
-  // @Patch(':id')
-  // update(@Param('id') id: string, @Body() updateUserDto: UpdateUserDto) {
-  //   return this.userService.update(+id, updateUserDto);
-  // }
+  @Put(':id')
+  update(@Param('id') id: string, @Body() updateUserDto: UpdateUserDto) {
+    return this.userService.update(id, updateUserDto);
+  }
 
-  // @Delete(':id')
-  // remove(@Param('id') id: string) {
-  //   return this.userService.remove(+id);
-  // }
+  @Delete(':id')
+  remove(@Param('id') id: string) {
+    return this.userService.remove(id);
+  }
 }
--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -1,8 +1,10 @@
 import { Injectable } from '@nestjs/common';
 import { CreateUserDto } from './dto/create-user.dto';
-// import { UpdateUserDto } from './dto/update-user.dto';
+import { UpdateUserDto } from './dto/update-user.dto';
 import { PrismaService } from '../prisma/prisma.service';
 import { Prisma } from '@prisma/client';
+
+// TODO: Need data validation for inproper input. Right now we only get a 500 error if the input is not valid.
 
 @Injectable()
 export class UserService {
@@ -38,19 +40,20 @@ export class UserService {
     });
   }
   
-  async findAll() {
+  findAll() {
     // Returns array of User objects --> User[]
-    return await this.prisma.user.findMany({
+    return this.prisma.user.findMany({
       select: {
         id: true,
         email: true, 
         full_name: true,
         role: true,
       }
-    });   }
+    });   
+  }
   
   async findOne(id: string) {
-    return this.prisma.user.findUnique({
+    const foundUser = await this.prisma.user.findUnique({
       where: { id },
       select: {
         id: true,
@@ -58,14 +61,49 @@ export class UserService {
         full_name: true,
         role: true,
       },
-    }) 
+    });
+
+    if (!foundUser) {
+      throw new Error('User not found'); // TODO: Still returns a 500 error, need to handle this properly
+    };
+
+    return foundUser;
   }
 
-  // update(id: number, updateUserDto: UpdateUserDto) {
-  //   return `This action updates a #${id} user`;
-  // }
+  async update(id: string, updateUserDto: UpdateUserDto) {
+    const updatedUser = await this.prisma.user.update({
+      where: { id },
+      data: updateUserDto,
+      select: {
+        id: true,
+        email: true,
+        full_name: true,
+        role: true,
+      },
+    });
 
-  // remove(id: number) {
-  //   return `This action removes a #${id} user`;
-  // }
+    return {
+      code: 200,
+      message: 'User updated successfully',
+      data: updatedUser
+    }
+  }
+
+  async remove(id: string) {
+    const deletedUser = await this.prisma.user.delete({
+      where: { id },
+      select: {
+        id: true,
+        email: true,
+        full_name: true,
+        role: true,
+      },
+    });
+
+    return {
+      code: 200,
+      message: 'User deleted successfully',
+      data: deletedUser,
+    }
+  }
 }
