f6d39df27a055b0315c31b46879a034a5baecc5a
Thomas Smallwood <tbm.smallwood@gmail.com>
2025-08-19 18:43:13 +0200

    initial auth commit

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,6 +5,7 @@ import { ConfigModule } from '@nestjs/config';
 import { PrismaModule } from './prisma/prisma.module';
 import { UserModule } from './user/user.module';
 import { TenantModule } from './tenant/tenant.module';
+import { AuthModule } from './auth/auth.module';
 
 @Module({
   imports: [
@@ -14,7 +15,8 @@ import { TenantModule } from './tenant/tenant.module';
     }), 
     PrismaModule,
     UserModule,
-    TenantModule
+    TenantModule,
+    AuthModule
   ],
   controllers: [AppController],
   providers: [AppService],
